191e798d6c75c2106974c3898de87190cb2003e0
trillobit3s <TrilloBit3s@gmail.com>
2026-05-26 16:13:46 -0300

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace PngParaIcoConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConverter = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnConverter
+            // 
+            this.btnConverter.Location = new System.Drawing.Point(62, 29);
+            this.btnConverter.Name = "btnConverter";
+            this.btnConverter.Size = new System.Drawing.Size(75, 23);
+            this.btnConverter.TabIndex = 0;
+            this.btnConverter.Text = "Converter";
+            this.btnConverter.UseVisualStyleBackColor = true;
+            this.btnConverter.Click += new System.EventHandler(this.btnConverter_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(203, 64);
+            this.Controls.Add(this.btnConverter);
+            this.Name = "Form1";
+            this.Text = "PNG para ICO";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConverter;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Forms;
+
+namespace PngParaIcoConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConverter_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.Filter = "PNG Image|*.png";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string pngPath = openFileDialog.FileName;
+
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+                saveFileDialog.Filter = "Icon File|*.ico";
+                saveFileDialog.Title = "Salvar ícone";
+
+                // Nome padrão baseado no PNG
+                saveFileDialog.FileName = Path.GetFileNameWithoutExtension(pngPath) + ".ico";
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string icoPath = saveFileDialog.FileName;
+
+                    ConvertTo32x32Ico(pngPath, icoPath);
+
+                    MessageBox.Show("Ícone criado em:\n" + icoPath);
+                }
+            }
+        }
+
+        public static void ConvertTo32x32Ico(string pngPath, string icoPath)
+        {
+            using (Bitmap original = new Bitmap(pngPath))
+            using (Bitmap resized = new Bitmap(32, 32))
+            {
+                using (Graphics g = Graphics.FromImage(resized))
+                {
+                    g.Clear(Color.Transparent);
+
+                    g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                    g.SmoothingMode = SmoothingMode.HighQuality;
+                    g.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                    g.DrawImage(original, 0, 0, 32, 32);
+                }
+
+                using (MemoryStream pngStream = new MemoryStream())
+                {
+                    resized.Save(pngStream, ImageFormat.Png);
+                    byte[] pngBytes = pngStream.ToArray();
+
+                    using (BinaryWriter writer = new BinaryWriter(File.Open(icoPath, FileMode.Create)))
+                    {
+                        // ICONDIR
+                        writer.Write((short)0);
+                        writer.Write((short)1);
+                        writer.Write((short)1);
+
+                        // ICONDIRENTRY
+                        writer.Write((byte)32);
+                        writer.Write((byte)32);
+                        writer.Write((byte)0);
+                        writer.Write((byte)0);
+                        writer.Write((short)1);
+                        writer.Write((short)32);
+                        writer.Write(pngBytes.Length);
+                        writer.Write(22);
+
+                        // PNG
+                        writer.Write(pngBytes);
+                    }
+                }
+            }
+        }
+    }
+}
